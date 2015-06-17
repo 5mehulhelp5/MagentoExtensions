10c04d027628c5e2d0946c9180683ae614b63a58
Mehul <5mehulhelp5@gmail.com>
2015-06-17 14:54:12 +0530

    New extension

---
+++ Guest Checkout/app/code/local/MehulChaudhari/GuestCheckout/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class MehulChaudhari_GuestCheckout_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ Guest Checkout/app/code/local/MehulChaudhari/GuestCheckout/Model/Observer.php
@@ -0,0 +1,20 @@
+<?php
+class MehulChaudhari_GuestCheckout_Model_Observer
+{
+
+			public function checkLogin(Varien_Event_Observer $observer)
+			{
+				$enable = (boolean)Mage::getStoreConfig('guestcheckout/settings/enable');
+				$msg = (string)Mage::getStoreConfig('guestcheckout/settings/msg');
+				if($enable){
+				        if(!Mage::getSingleton('customer/session')->isLoggedIn()){
+						            $returnUrl = Mage::helper('customer')->getLoginUrl();
+									Mage::getSingleton('core/session')->addError($msg);
+						            Mage::app()->getResponse()->setRedirect($returnUrl)->sendResponse();
+									exit;
+						}
+				}
+				
+			}
+		
+}
